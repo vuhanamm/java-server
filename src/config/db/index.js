8c2953b6b780c102611fa11c1bf518f21df939c3
vu_nguyen <vund.0709@gmail.com>
2025-04-23 22:31:15 +0700

    update db path

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,9 +4,9 @@ async function connect() {
     try {
         const url = 'mongodb+srv://java:9WBNRj9kxfT2qSqo@cluster0.ha1jjuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
         var a = await mongoose.connect(url);
-        console.log('Connect to db success')
+        console.log('Connect to db success '+url)
     } catch (e) {
-        console.log('Connect to db failed')
+        console.log('Connect to db failed '+e)
     }
 }
 
